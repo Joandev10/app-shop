68f9976680a0408bf43072853e9ad9b5cf6ceab4
Joandev10 <joan.dev93@gmail.com>
2021-09-04 12:56:26 -0500

    nueva funcionalidad-catalogo [commit desde feature/jpuma]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
